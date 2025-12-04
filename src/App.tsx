1f4e82bee12a11ed9036dbc2c9e2f944b9deb924
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2025-12-04 10:57:51 +0000

    probando libreria de markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import ReactMarkdown from "react-markdown";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,11 +12,25 @@ function App() {
     setCount(count+1)
   }
 
+  const markdownText = `
+  # Ejemplo
+
+  - Algo de texto
+  - Algo más texto
+
+  ## Subtitulo
+
+  ### Información adicional
+
+  Esto es un [link](https://github.com/remarkjs/react-markdown)
+  `;
+
   return (
     <>
       <h1> Hola mundo </h1>
       <button onClick={handleClick}>Pulsa</button>
       <span>{count}</span>
+      <ReactMarkdown>{markdownText}</ReactMarkdown>
     </>
   )
 }
